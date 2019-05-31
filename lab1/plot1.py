0b56a20c6f9567f65d8ec8c88a9c95e22f33d62a
dianaMess <ospanova2000@mail.ru>
2019-06-01 01:09:46 +0600

    fix gauss graph

--- lab1/plot1.py
+++ lab1/plot1.py
@@ -6,7 +6,7 @@ y1 = [0.04, 0.081, 0.098, 0.13, 0.28, 0.33]
 fig = plt.figure()
 ax = fig.add_subplot(211)
 ax.plot(x, y, label = u'Implementation', linewidth=5)
-ax.plot(x, y1, label = u'SciPy function', color='red', linewidth=10)
+ax.plot(x, y1, label = u'SciPy function', color='red', linewidth=5)
 ax.set_xlabel(u'Matrix size')
 ax.set_ylabel(u'Time')
 plt.legend()
